e86ac8999a5e5e2da9fe91e905790b45bcded8d0
MaKhay31 <147708795+Makhay31@users.noreply.github.com>
2025-02-12 21:13:03 +0200

    first attempt before tests

--- Lists.py
+++ Lists.py
@@ -7,27 +7,38 @@ def create_numbers_list():
     1.  Create a list named `numbers` containing the integers from 1 to 10 (inclusive).
         Return the numbers list
     """
-    
-
-    return 
+    for number in range(1,11):
+        numbers.append(number)
+    return numbers
 
 def add_eleven(numbers_list): # Modified to accept numbers_list as argument
     """
     2.  Add the number 11 to the end of the `numbers_list`.
     """
-    pass # Replace with your code
+    #pass # Replace with your code
+    numbers.append(11)
+    numbers_list = numbers
+    return numbers_list
 
 def insert_zero(numbers_list): # Modified to accept numbers_list as argument
     """
     3.  Insert the number 0 at the beginning of the `numbers_list`.
     """
-    pass # Replace with your code
+    #pass # Replace with your code
+    numbers.insert(0,0)
+    numbers_list = numbers
+    return numbers_list
 
 def remove_five(numbers_list): # Modified to accept numbers_list as argument
     """
     4.  Remove the number 5 from the `numbers_list`. (Remove the *value* 5, not the element at index 5).
     """
-    pass # Replace with your code
+    #pass # Replace with your code
+    for number in numbers:
+        if number == 5:
+            numbers.pop(numbers.index(5))
+    numbers_list = numbers
+    return numbers_list
 
 
 # Part 2: List Comprehension
@@ -38,14 +49,25 @@ def create_even_numbers(numbers_list): # Modified to accept numbers_list as argu
         from the `numbers_list` (created/modified in Part 1).
         Return even_numbers
     """
-    return None #Replace with your code
+    #return None #Replace with your code
+    even_numbers = []
+    for number in numbers :
+        if number % 2 == 0:
+            even_numbers.append(number)
+    numbers_list = numbers
+    return numbers_list
 
 def create_squared_numbers(numbers_list): # Modified to accept numbers_list as argument
     """
     2.  Using list comprehension, create a new list named `squared_numbers` containing the square of each number in
         the `numbers_list`. Return squared_numbers
     """
-    return None #Replace with your code
+    #return None #Replace with your code
+    squared_numbers =[]
+    for number in numbers:
+        squared_numbers.append(number**2)
+    numbers_list = numbers
+    return numbers_list
 
 # Part 3: String Slicing
 def extract_hello(text):
@@ -53,21 +75,24 @@ def extract_hello(text):
     1.  Given the string `text = "Hello, World!"`, extract the substring "Hello".
         Return the string.
     """
-    return None #Replace with your code
+    #return None #Replace with your code
+    return text[0:5]
 
 def extract_world(text):
     """
     2.  Given the string `text = "Hello, World!"`, extract the substring "World".
         Return the string
     """
-    return None #Replace with your code
+    #return None #Replace with your code
+    return text[7:12]
 
 def reverse_string(text):
     """
     3.  Given the string `text = "Hello, World!"`, create a new string that is the reversed version of the original string.
         Return the reversed string
     """
-    return None #Replace with your code
+    #return None #Replace with your code
+    return text[::-1]
 
 #Part 4: More list operations
 
@@ -75,18 +100,30 @@ def get_second_element(my_list):
     """
     1. Given a list called my_list, return the second element in the list, or None if it doesn't exist
     """
-    return None #Replace with your code
+    if len(my_list) >= 2:
+        return my_list[1]
+    else:
+        return None #Replace with your code
 
 def get_last_three_elements(my_list):
     """
     2. Given a list called my_list, return a new list consisting of the last three elements in my_list.
     If there are fewer than three elements, return a list with the elements that there are
     """
-    return None #Replace with your code
+    threes =list(my_list[len(my_list)-1:len(my_list)-4:-1])
+    if len(my_list) >= 3:
+        return threes
+    else:
+        return my_list
+        
+        
+    #return None #Replace with your code
 
 def replace_element(my_list, index, new_value):
     """
     3. Given a list called my_list, an integer index, and a new value, replace the element
        at the given index in my_list with the new value. Assume the list is valid
     """
-    pass #Replace with your code
+    my_list[index] == new_value
+    return my_list
+    #pass #Replace with your code
